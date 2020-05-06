432137ca565dd2760f5c2dfbcb89c88b8b7e642e
AnhKiet <letrananhkiet@gmail.com>
2020-05-06 18:00:43 +0700

    home page done

--- index.js
+++ index.js
@@ -1,12 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
-app.use(express.static('public'));
+const server = require('http').Server(app);
+
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Mongoose
+mongoose.connect('mongodb://localhost:27017/VGF', { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log("Database connected!");
+});
 app.get('/', function (req, res) {
     res.render('./index');
-})
+});
 
-app.listen(3000);
+/* 
+    FOR ADMIN 
+*/
+app.get('/admin/menu', function (req, res) {
+    res.render('./admin/menu');
+});
+
+
+// Config Server Port
+server.listen(3000, function () {
+    console.log('Web start at port 3000!!!');
+});
